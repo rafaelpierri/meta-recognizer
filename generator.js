47a85606a763a285b7118a43f5490dba5e49779c
Rafael Pierri <pierri.rafa@gmail.com>
2016-05-03 00:41:26 -0300

    New semantic routines for the Automaton Generator

--- generator.js
+++ generator.js
@@ -1,15 +1,19 @@
 'use strict';
 
 var RegexMap = require('./regexMap');
+var Automaton = require('./app');
 
 class Generator {
 
-  stack: [],
-  currentState: 1,
-  finalStates: [5],
-
-  automatonState: 0,
-  nextAutomatonState: 1,
+  constructor(automatonState, automatonNextState){
+    this.stack = [];
+    this.finalStates = [5];
+    //i dont know what i wrote up above
+    this.automatonState = automatonState || 0;
+    this.automatonNextState = automatonNextState || 1;
+    this.stateCounter = this.automatonNextState;
+    this.automaton = new Automaton(this.automatonState, [this.automatonNextState]);
+  }
 
   rules(token) {
     var nonTerminal = new RegExp('[A-Z]');
@@ -36,4 +40,57 @@ class Generator {
     };
   }
 
+  semantic00(){
+
+  }
+
+  semantic01(token){
+    this.automaton.addRule(this.automatonState, token, this.automatonNextState);
+    this.automatonState = this.automatonNextState;
+    this.automatonNextState++;
+  }
+  
+  semantic02(token){
+    this.stack.push({left: this.automatonState, right: this.automatonNextState});
+    this.automatonNextState++;
+  }
+
+  semantic02(token){
+    this.stack.push({left: this.automatonState, right: this.automatonNextState});
+    this.automatonNextState++;
+  }
+
+  semantic03(token){
+    this.automaton.addRule(this.automatonState, 'Ø', this.automatonNextState);
+    this.stack.push({left: this.automatonState, right: this.automatonNextState});
+    this.automatonNextState++;
+  }
+
+  semantic04(token){
+    this.automaton.addRule(this.automatonState, 'Ø', this.automatonNextState);
+    this.automatonState = this.automatonNextState;
+    this.stack.push({left: this.automatonState, right: this.automatonNextState});
+    this.automatonNextState++;
+  }
+  
+  semantic05(token){
+    var tuple = this.stack.pop();
+    this.automaton.addRule(this.automatonState, 'Ø', tuple.right);
+    this.automatonState = tuple.right;
+  }
+
+  semantic06(token){
+    var tuple = this.stack[this.stack.length - 1]
+    this.automaton.addRule(this.automatonState, 'Ø', tuple.right);
+    this.automatonState = tuple.left;
+  }
+
+  semantic07(token){
+    var tuple = this.stack.pop();
+    this.automaton.addRule(this.automatonState, 'Ø', tuple.right);
+    this.automatonState = tuple.right;
+    //TODO: make a submachine return (???)
+  }
 }
+
+module.exports = Generator;
