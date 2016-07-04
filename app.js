e7623742714bd2effcbc195bd97679842171281e
Rafael Pierri <pierri.rafa@gmail.com>
2016-07-04 08:52:41 -0300

    Corrections of Gram class

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var Gram = require('./gram');
 var app = function(string){
   var lexer = new Lexer();
   var tokens = lexer.tokenize(string); 
-  debugger;
   var grammar = new Gram(tokens); 
   grammar.consume();
   return grammar.generator.automaton;
--- gram.js
+++ gram.js
@@ -1,23 +1,20 @@
 'use strict';
 
 var Generator = require('./generator');
+var Expr = require('./expr');
 
 class Gram {
 
   constructor(tokens){
-    this.generator = null;
+    this.generator = new Generator();
     this.tokens = tokens;
     this.currentState = 1;
     this.rules = {
-      1 : { 'nonTerminal': {state: 2, callback: this.buildGenerator.bind(this) } },
-      2 : { 'assign': {state: 4, callback: this.generator.newDefinition.bind(this.generator)}},
-      4 : { 'dot': {state: 5, callback: this.generator.nonTerminalEnd.bind(this.generator)} },
+      1 : { 'nonTerminal': {state: 2, callback: () => {}}},
+      2 : { 'assign': {state: 4, callback: this.assignAndConsumeExpression.bind(this)}},
+      4 : { 'dot': {state: 5, callback: this.generator.nonTerminalEnd.bind(this.generator)}},
       5 : { 'nonTerminal': {state: 2, callback: ()=>{}} }
     };
-  }
-
-  buildGenerator(){
-    this.generator = new Generator();
   }
 
   changeState(token){
@@ -37,6 +34,12 @@ class Gram {
       var token = this.tokens.shift();
       this.changeState(token);
     }
+  }
+
+  assignAndConsumeExpression(token){
+    this.generator.newDefinirion(token);
+    var expr = new Expr(this.generator, this.tokens);
+    expr.consumeTokens();
   }
 
 }
